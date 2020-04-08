7acf1fee863518538e5059df7ab3f05e002971a5
alexlescinschi <48211961+alexlescinschi@users.noreply.github.com>
2020-04-08 16:20:53 +0300

    3

--- design-1/pages/index.vue
+++ design-1/pages/index.vue
@@ -6,7 +6,7 @@
         design-1
       </h1>
       <h2 class="subtitle">
-        My superior Nuxt.js project
+        Test-1 My superior Nuxt.js project
       </h2>
       <div class="links">
         <a
